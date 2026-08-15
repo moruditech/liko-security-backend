7c1f0b5f018d1206f455bd611a8be00a5875b671
Morudi Tech <moruditech@gmail.com>
2026-08-15 02:16:02 +0200

    Add files via upload

--- src/modules/auth/auth.validation.js
+++ src/modules/auth/auth.validation.js
@@ -3,7 +3,7 @@
 const Joi = require('joi');
 
 const login = Joi.object({
-  email: Joi.string().email().required(),
+  email: Joi.string().email({ tlds: { allow: false } }).required(),
   password: Joi.string().required(),
 });
 
@@ -16,7 +16,7 @@ const refresh = Joi.object({
 });
 
 const forgotPassword = Joi.object({
-  email: Joi.string().email().required(),
+  email: Joi.string().email({ tlds: { allow: false } }).required(),
 });
 
 const resetPassword = Joi.object({
